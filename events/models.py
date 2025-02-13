dbe488c947f17be64fdd66374032500215fbc83f
Amitha95 <99869312+Amitha95@users.noreply.github.com>
2025-02-13 17:11:41 +0530

    Added Gunicorn to requirements

--- events/models.py
+++ events/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    location = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to='events/')  # Optional event image
+
+    def __str__(self):
+        return self.name
--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Gallery
 
-# Register your models here.
+@admin.register(Gallery)
+class GalleryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at')  # Display these fields in admin
+    search_fields = ('title',)  # Search by title
