00e74ba72ba27550dd69049a93e2f5749856fd38
Amitha95 <99869312+Amitha95@users.noreply.github.com>
2025-02-13 17:11:41 +0530

    Added Gunicorn to requirements

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Event
 
-# Register your models here.
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date', 'location')  # Show these columns in admin
+    search_fields = ('name', 'location')  # Allow searching by event name/location
