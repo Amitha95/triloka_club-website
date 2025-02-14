3914dd76c1e79713680e18e527e1140e3e7ecf74
Amitha95 <99869312+Amitha95@users.noreply.github.com>
2025-02-14 12:20:36 +0530

    Fixed static file serving

--- backend/settings.py
+++ backend/settings.py
@@ -61,6 +61,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 ROOT_URLCONF = 'backend.urls'
 
